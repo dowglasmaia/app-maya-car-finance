a72119be11ccb293e2e5e8e7e746f49860bd959e
Dowglas Maia <dowglasmaia@gmail.com>
2021-07-20 23:06:46 -0300

    Criando e estilizando componente search

---
+++ src/app/pages/components/catalog-car/catalog-car.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-car',
+  templateUrl: './catalog-car.component.html',
+  styleUrls: ['./catalog-car.component.css']
+})
+export class CatalogCarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
